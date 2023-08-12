8ab730906ba647cefef548459d7dfa61550a1e09
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 11:50:57 -0700

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = i; j <= 9; j++)
+        {
+            if (i != j)
+            {
+                putchar(i + 48); /* 48 is the ASCII value of '0' */
+                putchar(j + 48);
+                if (i != 8 || j != 9) /* avoid printing comma and space for the last combination */
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
+}
