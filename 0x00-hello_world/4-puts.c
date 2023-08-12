dca4d1206a7ba4485eb5f84576d257965e4b609c
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 06:13:25 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-echo "Programming is like building a multilingual puzzle"; sed -i s/enable=false/enable=true/g /config/pqr.properties
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
