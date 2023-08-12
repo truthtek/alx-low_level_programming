dc509ace5175b5ee9b912fa90769eddad6a27bff
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 03:06:58 -0700

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Prints the size of various types on the computer it is compiled and run on.
+ * Warnings are allowed.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
