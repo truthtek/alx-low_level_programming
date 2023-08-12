e119614f2641abf7b213f384d43814f882da9e76
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 11:46:11 -0700

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int c;
+
+    for (c = 122; c >= 97; c--)
+        putchar(c);
+    putchar('\n');
+    return (0);
+}
