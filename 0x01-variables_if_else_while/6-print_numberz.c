34c3d05fe0cf7b79e888e0e6a1549e900f15b51d
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 11:45:02 -0700

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+
+    for (n = 0; n <= 9; n++)
+        putchar(n + 48); /* 48 is the ASCII value of '0' */
+    putchar('\n');
+    return (0);
+}
