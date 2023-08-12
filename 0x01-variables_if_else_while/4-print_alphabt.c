14e5775204e2dbed8ffb111d54aa2295258801a8
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 12:17:31 -0700

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-    int c;
+int c;
 
-    for (c = 97; c <= 122; c++)
-    {
-        if (c != 101 && c != 113)
-            putchar(c);
-    }
-    putchar('\n');
-    return (0);
+for (c = 97; c <= 122; c++)
+{
+if (c != 101 && c != 113)
+putchar(c);
+ }
+putchar('\n');
+return (0);
 }
