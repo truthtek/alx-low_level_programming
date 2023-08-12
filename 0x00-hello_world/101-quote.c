5eba3e8e6b9a1fb34e29e46aa32d8f82a6aa88af
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 03:32:24 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    char *message = "and that piece of art is useful\\\" - Dora Korpar, 2015-10-19\\n";
+    char *message = "and that piece of art is useful\\x22 - Dora Korpar, 2015-10-19\\x0a";
     ssize_t len = sizeof(message) - 1;
 
     write(STDERR_FILENO, message, len);
