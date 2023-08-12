668a4b6ca0f0a357f86934993469a716bbfffe2d
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 11:58:38 -0700

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,21 +8,16 @@
 int main(void)
 {
     int i, j;
-    int putchar(int c);
 
     for (i = 0; i <= 9; i++)
     {
+        putchar(i + 48); /* print the first number */
         for (j = i; j <= 9; j++)
         {
             if (i != j)
             {
-                putchar(i + 48); /* 48 is the ASCII value of '0' */
-                putchar(j + 48);
-                if (i != 8 || j != 9) /* avoid printing comma and space for the last combination */
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
+                putchar((i << 8) | ',');
+                putchar((j << 8) | ' ');
             }
         }
     }
