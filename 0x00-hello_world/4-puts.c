d98af91c64c9325ccb013743f3a4fb22d1a054b9
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 02:50:07 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the specified text followed by a new line using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
+    puts("Programming is like building a multilingual puzzle");
     return (0);
 }
