bdf5e751538d0c6505d4fe033a78d524efe12cc6
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 12:18:13 -0700

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-    int c;
+int c;
 
-    for (c = 122; c >= 97; c--)
-        putchar(c);
-    putchar('\n');
-    return (0);
+for (c = 122; c >= 97; c--)
+putchar(c);
+putchar('\n');
+return (0);
 }
