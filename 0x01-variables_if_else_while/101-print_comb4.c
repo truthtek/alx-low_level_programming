44bc1b50fb4d4e1b8597d75d40d780445f1bc3f9
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 12:12:33 -0700

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int hundreds_digit;
+    int tens_digit;
+    int ones_digit;
+
+    for (hundreds_digit = 0; hundreds_digit < 8; hundreds_digit++)
+    {
+        for (tens_digit = hundreds_digit + 1; tens_digit < 9; tens_digit++)
+        {
+            for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+            {
+                putchar(hundreds_digit + '0');
+                putchar(tens_digit + '0');
+                putchar(ones_digit + '0');
+
+                if (hundreds_digit != 7 || tens_digit != 8 || ones_digit != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
+}
