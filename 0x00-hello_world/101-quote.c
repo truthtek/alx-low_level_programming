87244ea6ac3e0b6d57105e705e017e453ed04107
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 03:37:26 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+/* Prints a quote to the standard error */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
