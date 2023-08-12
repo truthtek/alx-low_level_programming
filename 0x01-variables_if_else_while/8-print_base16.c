256372848b85bc332e709942fbd7a1359de9c62e
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 11:47:22 -0700

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+    char c;
+
+    for (n = 48; n < 58; n++) /* 48 is the ASCII value of '0' */
+        putchar(n);
+    for (c = 'a'; c <= 'f'; c++)
+        putchar(c);
+    putchar('\n');
+    return (0);
+}
