e9ea9f26dd7fc8436fd2b92b3babe1b243fb553c
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 02:40:47 -0700

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/* Prints a sentence with proper grammar */
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the specified text followed by a new line.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
