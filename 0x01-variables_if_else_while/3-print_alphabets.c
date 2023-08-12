8c9d8c837fe653e437fc490aef8591507debe96a
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-12 12:16:58 -0700

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    int c;
+int c;
 
-    for (c = 97; c <= 122; c++)
-        putchar(c);
-    for (c = 65; c <= 90; c++)
-        putchar(c);
-    putchar('\n');
-    return (0);
+for (c = 97; c <= 122; c++)
+putchar(c);
+for (c = 65; c <= 90; c++)
+putchar(c);
+putchar('\n');
+return (0);
 }
