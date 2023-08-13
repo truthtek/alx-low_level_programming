51412198b671a4dd3d6cc2eab4afe7481ac9b9f1
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-13 07:00:35 -0700

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,30 +1,18 @@
-#include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
-    int i = 0;
+    int i;
 
-    while (i < 10)
+    for (i = 0; i <= 9; i++)
     {
-        int j = i + 1;
-
-        while (j < 10)
+        putchar(i + '0');
+        if (i != 9)
         {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
             putchar(',');
             putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
-
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-            j++;
         }
-        i++;
     }
+    putchar('\n');
     return (0);
 }
