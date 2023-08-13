26f0df429207adae8fe36de8767ed01c8cfe8e70
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-13 07:15:50 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,7 @@
  *
  * : This program uses two nested for loops to iterate through all
  * possible combinations of two digits. The outer loop iterates through the
- * first digit (i) from 0 to 9, while the inner loop iterates through the second
+ * first digit (i) from 0 to 9,while the inner loop iterates through the second
  * digit (j) from i + 1 to 9. In each iteration of the inner loop, the program
  * uses the putchar function to print the current combination of digit (i + '0'
  * and j + '0') and, if the current combination is not the last one (i != 8 ||
@@ -21,13 +21,13 @@ int i, j;
 for (i = 0; i <= 9; i++)
 {
 for (j = i + 1; j <= 9; j++)
- {
-     putchar(i + '0');
-     putchar(j + '0');
+{
+putchar(i + '0');
+putchar(j + '0');
 if (i != 8 || j != 9)
 {
-    putchar(',');
-    putchar(' ');
+putchar(',');
+putchar(' ');
 }
 }
 }
