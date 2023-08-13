155549fa1874dca98fe448bb07f779da2a3d34ee
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-13 07:44:14 -0700

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * : This program uses two nested for loops to iterate through all
+ * possible combinations of two digits. The outer loop iterates through the
+ * first digit (i) from 0 to 9,while the inner loop iterates through the second
+ * digit (j) from i + 1 to 9. In each iteration of the inner loop, the program
+ * uses the putchar function to print the current combination of digit (i + '0'
+ * and j + '0') and, if the current combination is not the last one (i != 8 ||
+ * j != 9), a comma and a space. Finally, after both loops, the program uses
+ * putchar to print a newline character.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
