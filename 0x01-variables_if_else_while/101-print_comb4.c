d3af9ac515182643ebb9f035e626df356697cc1c
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-13 07:52:30 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,31 +2,25 @@
 
 int main(void)
 {
-int i, j, k;
+    int i, j, k;
 
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-if (i != j)
-{
-for (k = 0; k < 10; k++)
-{
-if (k != i && k != j)
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(k + '0');
-if (i < 7)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
+    for (i = 0; i < 10; i++)
+    {
+        for (j = i + 1; j < 10; j++)
+        {
+            for (k = j + 1; k < 10; k++)
+            {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+                if (i != 7 || j != 8 || k != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
 }
