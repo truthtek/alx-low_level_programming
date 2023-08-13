b3fc9289a6ac799fb45163c04c6e1099889169cd
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-13 07:20:31 -0700

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+
+    for (i = 0; i <= 99; i++)
+    {
+        for (j = i; j <= 99; j++)
+        {
+            if (i != j)
+            {
+                putchar(i / 10 + '0');
+                putchar(i % 10 + '0');
+                putchar(' ');
+                putchar(j / 10 + '0');
+                putchar(j % 10 + '0');
+
+                if (i != 98 || j != 99)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
+}
