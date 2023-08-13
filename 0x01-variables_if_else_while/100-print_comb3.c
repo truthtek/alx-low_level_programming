bb9e8953923dde6c012198e759f3b0a6ad287464
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-13 07:08:53 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all possible combinations of two-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+putchar(i + '0');   
+putchar(j + '0');   
+            
+            
+if (i != 8 || j != 9)
+{
+putchar(',');     // Print comma
+putchar(' ');     // Print space
 }
+}
+}
+putchar('\n');             // Print newline
+return (0);
+}
+
