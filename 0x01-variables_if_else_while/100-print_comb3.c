bc068b81d3c2fbfaac7616f8116549d24487b989
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-13 07:13:30 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@
  * possible combinations of two digits. The outer loop iterates through the
  * first digit (i) from 0 to 9, while the inner loop iterates through the second
  * digit (j) from i + 1 to 9. In each iteration of the inner loop, the program
- * uses the putchar function to print the current combination of digits (i + '0'
+ * uses the putchar function to print the current combination of digit (i + '0'
  * and j + '0') and, if the current combination is not the last one (i != 8 ||
  * j != 9), a comma and a space. Finally, after both loops, the program uses
  * putchar to print a newline character.
@@ -16,21 +16,21 @@
  */
 int main(void)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+ {
+     putchar(i + '0');
+     putchar(j + '0');
+if (i != 8 || j != 9)
+{
+    putchar(',');
+    putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
