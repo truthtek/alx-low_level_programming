908422698e659337cd3af5b10e4dd127b672d419
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 05:47:45 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,50 @@
 #include "main.h"
 
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The sum of a and b
+ */
+
 void print_times_table(int n)
 {
-    int i, j, product;
+int i, j, product;
 
-    if (n < 0 || n > 15)
-        return;
+if (n < 0 || n > 15)
+return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            product = i * j;
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            if (product < 10 && j != 0)
-            {
-                _putchar(' ');
-                _putchar(' ');
-            }
-            if (product >= 100)
-            {
-                _putchar(product / 100 + '0');
-                _putchar((product / 10) % 10 + '0');
-                _putchar(product % 10 + '0');
-            }
-            else if (product >= 10)
-            {
-                _putchar(' ');
-                _putchar(product / 10 + '0');
-                _putchar(product % 10 + '0');
-            }
-            else
-                _putchar(product + '0');
-        }
-        _putchar('\n');
-    }
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+product = i * j;
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (product < 10 && j != 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+if (product >= 100)
+{
+_putchar(product / 100 + '0');
+_putchar((product / 10) % 10 + '0');
+_putchar(product % 10 + '0');
+}
+else if (product >= 10)
+{
+_putchar(' ');
+_putchar(product / 10 + '0');
+_putchar(product % 10 + '0');
+}
+else
+ _putchar(product + '0');
+}
+_putchar('\n');
+}
 }
