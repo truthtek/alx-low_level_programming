08d997c069e3d9fe0f2bc7cdb40cdb8f7158608e
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 03:03:21 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ */
 void print_alphabet_x10(void)
 {
+    char letter;
     int i;
-    char c;
 
     for (i = 0; i < 10; i++)
     {
-        for (c = 'a'; c <= 'z'; c++)
-            _putchar(c);
+        letter = 'a';
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
         _putchar('\n');
     }
 }
