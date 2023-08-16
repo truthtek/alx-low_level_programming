c8b327c1773ffa785f6121f827d956c1ab1ede94
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 06:14:06 -0700

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - Prints the Fibonacci sequence up to the 50th term
+ * main - Finds and prints the sum of even-valued terms in the Fibonacci sequence
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-long int i, t1 = 1, t2 = 2, nextTerm;
+    long int t1 = 1, t2 = 2, nextTerm;
+    long int sum = 0;
 
-printf("%ld, %ld", t1, t2);
-for (i = 3; i <= 50; ++i)
-{
-nextTerm = t1 + t2;
-printf(", %ld", nextTerm);
-t1 = t2;
-t2 = nextTerm;
-}
-printf("\n");
-return (0);
+    while (t2 <= 4000000)
+    {
+        if (t2 % 2 == 0)
+        {
+            sum += t2;
+        }
+
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    printf("%ld\n", sum);
+
+    return 0;
 }
