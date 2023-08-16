15c98c746fb5e93601ec5bd3ad5ff2341aecd9d8
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 04:15:31 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -49,13 +49,3 @@ void print_times_table(int n)
         _putchar('\n');
     }
 }
-
-
-
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_times_table(int n);
-
-#endif /* MAIN_H */
