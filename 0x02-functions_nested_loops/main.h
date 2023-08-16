895a87abe0cc338c9f69434a06349ee47ba24d42
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 05:32:20 -0700

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,84 +3,36 @@
 
 void print_to_98(int n);
 
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void print_times_table(int n);
-
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 void print_alphabet_x10(void);
 
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 int add(int a, int b);
-
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 void jack_bauer(void);
 
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 int print_last_digit(int n);
-
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 int _abs(int n);
 
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 int print_sign(int n);
-
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 int _isalpha(int c);
 
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void print_alphabet(void);
-
-#endif /* MAIN_H */
-
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
 
 #endif /* MAIN_H */
+
+
