10c54183b5f9b82aca7f29ac819306400d09d0ea
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 03:07:39 -0700

    Update main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    'print_alphabet_x10'; // Call the function to print the alphabet 10 times
-    return 0;
+    print_alphabet_x10(); // Call the function to print the alphabet 10 times
+    return (0);
 }
