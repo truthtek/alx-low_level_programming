18fc4bbba2f8a4a2ace022f53d5c06760f07bb3b
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 04:14:48 -0700

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the n times table
+ * @n: The number for which the times table is printed
+ */
+void print_times_table(int n)
+{
+    if (n < 0 || n > 15)
+        return;
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            int product = i * j;
+
+            if (j != 0)
+            {
+                _putchar(' ');
+                if (product < 100)
+                    _putchar(' ');
+                if (product < 10)
+                    _putchar(' ');
+            }
+
+            if (product >= 100)
+            {
+                _putchar(product / 100 + '0');
+                _putchar((product / 10) % 10 + '0');
+            }
+            else if (product >= 10)
+            {
+                _putchar(' ');
+                _putchar(product / 10 + '0');
+            }
+            else
+            {
+                if (j != 0)
+                    _putchar(' ');
+                _putchar(' ');
+            }
+
+            _putchar(product % 10 + '0');
+
+            if (j != n)
+                _putchar(',');
+        }
+        _putchar('\n');
+    }
+}
+
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_times_table(int n);
+
+#endif /* MAIN_H */
