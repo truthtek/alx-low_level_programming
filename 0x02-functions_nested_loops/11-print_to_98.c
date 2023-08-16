6d8c2b39a988154e9c54f0d2ee5effa5dbb9a2a0
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 03:59:35 -0700

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints natural numbers from n to 98
+ * @n: The starting number
+ */
+void print_to_98(int n)
+{
+    if (n <= 98)
+    {
+        for (int i = n; i <= 98; i++)
+        {
+            printf("%d", i);
+            if (i != 98)
+                printf(", ");
+        }
+    }
+    else
+    {
+        for (int i = n; i >= 98; i--)
+        {
+            printf("%d", i);
+            if (i != 98)
+                printf(", ");
+        }
+    }
+    printf("\n");
+}
