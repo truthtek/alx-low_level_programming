8206484029f01b5e21850689abdfe2c5fee53d78
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-16 03:50:50 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,9 @@ for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-product = i * j;
-            
+product = i * j;           
 if (j != 0)
 _putchar(' ');
-
 if (product >= 10)
 {
 _putchar(product / 10 + '0');
