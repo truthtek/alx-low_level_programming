d1e1af8be34660a93a0cb6dd273638ab3846703a
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-18 14:23:46 -0700

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
 * _isdigit - Checks if a character is a digit (0 through 9).
-* c
+* @c: The character to be checked.
+*
 * Return: 1 if c is a digit, 0 otherwise.
 */
 int _isdigit(int c)
