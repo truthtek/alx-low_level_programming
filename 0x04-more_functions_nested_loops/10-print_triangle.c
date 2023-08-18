b022569bf7f95966eb4aedf0d07218014d77756d
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-18 13:36:49 -0700

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,28 @@
  */
 void print_triangle(int size)
 {
-int row, col;
-if (size <= 0)
-{
-_putchar('\n');
-return;
+int row, col, spaces;
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (row = 1; row <= size; row++)
+    {
+        spaces = size - row;
+
+        for (col = 0; col < spaces; col++)
+        {
+            _putchar(' ');
+        }
+
+        for (col = 0; col < row; col++)
+        {
+            _putchar('#');
+        }
+
+        _putchar('\n');
+    }
 }
 
-for (row = 1; row <= size; row++)
-{
-for (col = 1; col <= row; col++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
