76390b538aa3cd1d904f58b5be68da2bb2bb8a97
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-18 01:20:36 -0700

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,19 +1,24 @@
 int largest_number(int a, int b, int c)
+
+/**
+ * main - tests function that prints if integer is positive, negative, or zero
+ * Return: 0
+ */
 {
-    int largest;
+int largest;
 
-    if (a >= b && a >= c)
-    {
-        largest = a;
-    }
-    else if (b >= a && b >= c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+if (a >= b && a >= c)
+{
+largest = a;
+}
+else if (b >= a && b >= c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-    return (largest);
+return (largest);
 }
