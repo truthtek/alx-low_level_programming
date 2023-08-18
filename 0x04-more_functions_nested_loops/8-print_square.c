e5a5e1605dfec7a43242d71d2c11a36c17bb5445
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-18 13:22:40 -0700

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,20 +6,18 @@
  */
 void print_square(int size)
 {
-    int row, col;
-
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-
-    for (row = 0; row < size; row++)
-    {
-        for (col = 0; col < size; col++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+int row, col;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (row = 0; row < size; row++)
+{
+for (col = 0; col < size; col++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
