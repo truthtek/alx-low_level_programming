96fa71df262780859e38a57dc747ddbfd5b6aa46
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-18 13:38:36 -0700

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_triangle - Prints a triangle of hash symbols in the terminal.
  * @size: The size of the triangle.
@@ -7,27 +6,23 @@
 void print_triangle(int size)
 {
 int row, col, spaces;
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-
-    for (row = 1; row <= size; row++)
-    {
-        spaces = size - row;
-
-        for (col = 0; col < spaces; col++)
-        {
-            _putchar(' ');
-        }
-
-        for (col = 0; col < row; col++)
-        {
-            _putchar('#');
-        }
-
-        _putchar('\n');
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (row = 1; row <= size; row++)
+{
+spaces = size - row;
+for (col = 0; col < spaces; col++)
+{
+_putchar(' ');
+}
+for (col = 0; col < row; col++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
 
