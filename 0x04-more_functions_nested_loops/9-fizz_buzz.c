12d5cea1a949894fef637da1bb6c2fe678bd0f80
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-18 13:16:30 -0700

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints numbers from 1 to 100 with Fizz-Buzz rules.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int num;
+
+    for (num = 1; num <= 100; num++)
+    {
+        if (num % 3 == 0 && num % 5 == 0)
+            printf("FizzBuzz");
+        else if (num % 3 == 0)
+            printf("Fizz");
+        else if (num % 5 == 0)
+            printf("Buzz");
+        else
+            printf("%d", num);
+
+        if (num != 100)
+            printf(" ");
+        else
+            printf("\n");
+    }
+
+    return (0);
+}
