684253058be99b8f500055e7c3ca726366bae31b
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-18 13:24:15 -0700

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,22 +6,20 @@
  */
 void print_diagonal(int n)
 {
-    int spaces = 0;
-
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-
-    while (n > 0)
-    {
-        int i;
-        for (i = 0; i < spaces; i++)
-            _putchar(' ');
-        _putchar('\\');
-        _putchar('\n');
-        spaces++;
-        n--;
-    }
+int spaces = 0;
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+while (n > 0)
+{
+int i;
+for (i = 0; i < spaces; i++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+spaces++;
+n--;
+}
 }
