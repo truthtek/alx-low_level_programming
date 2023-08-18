bcf21a55457befaaae7a10f982a491f01e61ee3e
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-18 13:10:41 -0700

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line of backslashes in the terminal.
+ * @n: The number of times the character '\' should be printed.
+ */
+void print_diagonal(int n)
+{
+    int spaces = 0;
+
+    if (n <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    while (n > 0)
+    {
+        int i;
+        for (i = 0; i < spaces; i++)
+            _putchar(' ');
+        _putchar('\\');
+        _putchar('\n');
+        spaces++;
+        n--;
+    }
+}
