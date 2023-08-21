c50d969b110445444757886ee5f72efc45de82cf
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 06:50:40 -0700

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the string.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s) {
+    int sign = 1; 
+    int result = 0;
+    int i = 0;
+    
+   
+    if (s[i] == '-') {
+        sign = -1;
+        i++;
+    } else if (s[i] == '+') {
+        i++;
+    }
+    
+   
+    while (s[i] != '\0') {
+        if (s[i] >= '0' && s[i] <= '9') {
+            result = result * 10 + (s[i] - '0');
+        } else {
+            break;
+        }
+        i++;
+    }
+    
+    return result * sign;
+}
