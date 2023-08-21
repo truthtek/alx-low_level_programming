16b35b77c249b243ce0caa8524001411f88f4da5
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 06:04:11 -0700

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string.
+ * @str: Pointer to the string.
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
+}
