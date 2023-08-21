43ab966b5819c00095cbbaaca92f295a1137d94f
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 06:34:44 -0700

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string followed by a new line.
+ * @str: Pointer to the string.
+ */
+void puts_half(char *str)
+{
+int length = 0;
+int start;
+while (str[length] != '\0')
+{
+length++;
+}
+start = (length - 1) / 2 + 1;
+while (str[start] != '\0')
+{
+_putchar(str[start]);
+start++;
+}
+_putchar('\n');
+}
