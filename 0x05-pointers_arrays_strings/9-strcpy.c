d2d758dc93d82c93a25b1666520b3fb70b0f03f1
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 06:48:12 -0700

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src to the buffer pointed to by dest.
+ * _strcpy - Copies the string pointed to by src to the buffer to by dest.
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string.
  *
