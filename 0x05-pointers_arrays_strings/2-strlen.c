1bdec11b0af0c99c882172f9c87f7351c912ab29
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 01:38:26 -0700

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Computes the length of a string.
+ * @s: Pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return length;
+}
