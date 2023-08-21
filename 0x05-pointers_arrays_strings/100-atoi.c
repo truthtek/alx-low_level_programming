b87b2a5e005d6105dba7c1fde9a6be15f2a20980
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 06:54:20 -0700

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,28 +6,25 @@
  *
  * Return: The converted integer.
  */
-int _atoi(char *s) {
-    int sign = 1; 
+int _atoi(char *s)
+{
+    int sign = 1;
     int result = 0;
     int i = 0;
-    
-   
-    if (s[i] == '-') {
-        sign = -1;
-        i++;
-    } else if (s[i] == '+') {
-        i++;
-    }
-    
-   
-    while (s[i] != '\0') {
-        if (s[i] >= '0' && s[i] <= '9') {
-            result = result * 10 + (s[i] - '0');
-        } else {
-            break;
-        }
+
+    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
         i++;
     }
-    
-    return result * sign;
+
+    while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+    {
+        result = result * 10 + sign * (s[i] - '0');
+        i++;
+    }
+
+    return result;
 }
+
