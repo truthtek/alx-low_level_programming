92e6a2dbebc789a3fe22f58173970a194804d84d
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 06:06:31 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string.
- * @str: Pointer to the string.
+ * print_string_2 - prints every other character of a string
+ * @str: the string to print
+ *
+ * Return: void
  */
-void puts2(char *str)
+void print_string_2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i += 2;
-}
-_putchar('\n');
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (i % 2 == 0)
+        {
+            _putchar(str[i]);
+        }
+    }
+    _putchar('\n');
 }
