b8e5d946f98d2eba2c246d36877af07d8e0d8915
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 06:44:47 -0700

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints n elements of an array of integers followed by a new line.
+ * print_array - Prints n elements of an array of integers followed by a line.
  * @a: Pointer to the array of integers.
  * @n: Number of elements to print.
  */
