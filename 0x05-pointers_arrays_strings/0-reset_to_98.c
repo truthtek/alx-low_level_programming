b7082bae97aa660cf03871e7dbf04bdeec9b7110
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 01:30:40 -0700

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,5 +7,5 @@
  * This function updates the value pointed to by the given pointer to 98.
  */
 void reset_to_98(int *n) {
-*n = 98; // Update the value pointed to by the pointer to 98
+*n = 98; /* Update the value pointed to by the pointer to 98 */
 }
