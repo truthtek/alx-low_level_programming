be6559fe3d33bfa09c910c8c12f8c0d6e7b87101
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 01:56:34 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,16 +4,19 @@
  * print_rev - Prints a string in reverse followed by a new line.
  * @s: Pointer to the string.
  */
-void print_rev(char *s)
-{
-int length = 0;
-while (s[length] != '\0')
-{
-length++;
-}
-for (int i = length - 1; i >= 0; i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
+void print_rev(char *s) {
+    int length = 0;
+    
+   
+    while (s[length] != '\0') {
+        length++;
+    }
+    
+ 
+    int i;
+    for (i = length - 1; i >= 0; i--) {
+        _putchar(s[i]);
+    }
+    
+    _putchar('\n');
 }
