2584844eb62ca1af02067269ffcc69197ecba183
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-21 02:03:33 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,15 @@ void print_rev(char *s)
 {
 int length = 0;
 int i; 
-    /* Calculate the length of the string */
+/* Calculate the length of the string */
 while (s[length] != '\0')
 {
 length++;
-}
-    
-    /* Print the string in reverse */
-for (i = length - 1; i >= 0; i--) {
- _putchar(s[i]);
+}  
+/* Print the string in reverse */
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
 }
 _putchar('\n'); /* Print a new line after the reversed string */
 }
