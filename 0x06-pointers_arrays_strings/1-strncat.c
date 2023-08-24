dd76833287c26fbeb5259e33272dbbbfd0430134
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-24 00:58:16 -0700

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,11 +9,11 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i, j;
-    for (i = 0; dest[i] != '\0'; i++)
-        ;
-    for (j = 0; j < n && src[j] != '\0'; j++)
-        dest[i + j] = src[j];
-    dest[i + j] = '\0';
-    return (dest);
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (j = 0; j < n && src[j] != '\0'; j++)
+dest[i + j] = src[j];
+dest[i + j] = '\0';
+return (dest);
 }
