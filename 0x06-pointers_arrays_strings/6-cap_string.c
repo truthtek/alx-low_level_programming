80c92e36c5cf13d7d50755c7da7fc0551d49dc16
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-24 01:16:08 -0700

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,47 @@
 #include "main.h"
 
 /**
- * string_toupper - Converts all lowercase letters in a string to uppercase.
+ * is_separator - Checks if a character is a word separator.
+ * @c: The character to check.
+ * Return: 1 if c is a separator, 0 otherwise.
+ */
+int is_separator(char c)
+{
+    char separators[] = " \t\n,;.!?\"(){}";
+    int i;
+
+    for (i = 0; separators[i] != '\0'; i++)
+    {
+        if (separators[i] == c)
+            return 1;
+    }
+    return 0;
+}
+
+/**
+ * cap_string - Capitalizes all words of a string.
  * @str: The input string.
  * Return: A pointer to the modified string.
  */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
     char *ptr = str;
+    int capitalize_next = 1;
 
     while (*ptr != '\0')
     {
-        if (*ptr >= 'a' && *ptr <= 'z')
+        if (is_separator(*ptr))
+        {
+            capitalize_next = 1;
+        }
+        else if (capitalize_next && (*ptr >= 'a' && *ptr <= 'z'))
         {
             *ptr = *ptr - ('a' - 'A'); // Convert to uppercase
+            capitalize_next = 0;
+        }
+        else
+        {
+            capitalize_next = 0;
         }
         ptr++;
     }
