4ad37cab8633264a03a22e5cfba746c06039240e
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-24 15:17:40 -0700

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program.
- *
- * Description: This program demonstrates memory manipulation to change
- * the value of a[2] using pointer arithmetic without directly modifying a.
- * It prints the value of a[2] before and after the change.
- *
- * Return: Always 0 (success)
- */
 int main(void)
 {
-    int n;
-    int a[5];
-    int *p;
+  int n;
+  int a[5];
+  int *p;
 
-    a[2] = 1024;
-    p = &n;
-    *(p + 5) = 98;  /* Manipulate memory to change a[2] to 98 */
-    printf("a[2] = %d\n", *(p + 5)); /* Print modified a[2] */
-    printf("");
-    printf("a[2] = %d\n", a[2]); /* Print original a[2] */
-    return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
