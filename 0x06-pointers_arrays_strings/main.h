c9cc675158cbc8df4d9da1bdcd4e9cacb109d040
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-24 00:49:14 -0700

    Create main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for _strcat */
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
