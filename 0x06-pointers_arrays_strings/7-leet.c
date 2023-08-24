df5f09e7d0a37e58e080e76aa7ee5145086909f8
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-24 01:22:04 -0700

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: the string to encode
+ *
+ * Return: char pointer
+ */
+char *leet(char *s)
+{
+int i, j;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "43071";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; letters[j] != '\0'; j++)
+{
+if (s[i] == letters[j])
+s[i] = numbers[j / 2];
+}
+}
+return (s);
+}
