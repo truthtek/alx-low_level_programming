351119a03f31a6ecf4493ed6c1f64cdc6012aaf7
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-24 02:02:37 -0700

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,34 +6,30 @@
  */
 void print_number(int n)
 {
-    unsigned int num;
-    int divisor;
-    int place_value;
-
-    if (n < 0)
-    {
-        _putchar('-');
-        num = -n;
-    }
-    else
-    {
-        num = n;
-    }
-
-    place_value = 1;
-    divisor = 1;
-
-    while (num / divisor >= 10)
-    {
-        divisor *= 10;
-        place_value++;
-    }
-
-    while (place_value > 0)
-    {
-        _putchar((num / divisor) + '0');
-        num %= divisor;
-        divisor /= 10;
-        place_value--;
-    }
+unsigned int num;
+int divisor;
+int place_value;
+if (n < 0)
+{
+_putchar('-');
+num = -n;
+}
+else
+{
+num = n;
+}
+place_value = 1;
+divisor = 1;
+while (num / divisor >= 10)
+{
+divisor *= 10;
+place_value++;
+}
+while (place_value > 0)
+{
+_putchar((num / divisor) + '0');
+num %= divisor;
+divisor /= 10;
+place_value--;
+}
 }
