edf2538d55321693d0513109a2c05f77ebc01b0a
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-24 01:01:54 -0700

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ int _putchar(char c);
 /* Function prototype for _strcat */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
