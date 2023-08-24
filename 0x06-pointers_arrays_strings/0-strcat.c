3b83ad1e0ff2d4e38f31044e1a9875174823144d
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-24 15:09:10 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string to append.
- * Return: A pointer to the resulting string dest.
- */
+* _strcat - Concatenates two strings.
+* @dest: The destination string.
+* @src: The source string to append.
+* Return: A pointer to the resulting string dest.
+*/
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+int i,j;
 for (i = 0; dest[i] != '\0'; i++)
-       {
+{
 ;
 }
 for (j = 0; src[j] != '\0'; j++)
