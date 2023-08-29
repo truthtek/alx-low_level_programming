6f510a93d85fcaf3452806b23d982a80f984ba51
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-29 13:23:22 -0700

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,11 +9,11 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-            printf("%c", a[i][j]);
-        printf("\n");
-    }
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+printf("%c", a[i][j]);
+printf("\n");
+}
 }
