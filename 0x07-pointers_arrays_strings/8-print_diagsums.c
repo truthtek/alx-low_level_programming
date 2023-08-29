9213867e99d24472c1115c02d9a0bd8c977b11f7
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-29 13:28:52 -0700

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,12 +10,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int i, sum1 = 0, sum2 = 0;
-    for (i = 0; i < size; i++)
-    {
-        sum1 += *(a + i * size + i);
-        sum2 += *(a + (size - 1) * (i + 1));
-    }
-    printf("%d, %d\n", sum1, sum2);
+int i, sum1 = 0, sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum1 += *(a + i * size + i);
+sum2 += *(a + (size - 1) * (i + 1));
+}
+printf("%d, %d\n", sum1, sum2);
 }
 
