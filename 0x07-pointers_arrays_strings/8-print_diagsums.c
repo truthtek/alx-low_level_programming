33edb72ba00bad2dea2dc7c22d87476ebae73e29
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-29 13:28:10 -0700

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,18 +8,14 @@
  *
  * Return: None.
  */
-void print_diagsums(int *a, int size) {
-    int diag1_sum = 0;
-    int diag2_sum = 0;
-
-    for (int i = 0; i < size; i++) {
-        // Add the elements of the main diagonal
-        diag1_sum += *(a + i*size + i);
-
-        // Add the elements of the secondary diagonal
-        diag2_sum += *(a + i*size + (size - 1 - i));
+void print_diagsums(int *a, int size)
+{
+    int i, sum1 = 0, sum2 = 0;
+    for (i = 0; i < size; i++)
+    {
+        sum1 += *(a + i * size + i);
+        sum2 += *(a + (size - 1) * (i + 1));
     }
-
-    printf("Sum of main diagonal: %d\n", diag1_sum);
-    printf("Sum of secondary diagonal: %d\n", diag2_sum);
+    printf("%d, %d\n", sum1, sum2);
 }
+
