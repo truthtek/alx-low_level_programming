dbca8ae7942221e54991488b8817557d5f2f2e4c
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-30 14:08:14 -0700

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,30 +6,31 @@
  *
  * Return: 1 if s is a palindrome, 0 otherwise
  */
- int is_palindrome_helper(char *s, int start, int end)
+int is_palindrome_helper(char *s, int start, int end)
 {
+/* function body */
 if (start >= end)
 {
-return 1;
+return (1);
 }
 if (s[start] != s[end])
 {
-return 0;
+return (0);
 }
-return is_palindrome_helper(s, start + 1, end - 1);
+return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 int _strlen_recursion(char *s)
 {
+/* function body */
 if (*s == '\0')
 {
-return 0;
+return (0);
 }
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
-
 int is_palindrome(char *s)
 {
 int len = _strlen_recursion(s);
-return is_palindrome_helper(s, 0, len - 1);
+return (is_palindrome_helper(s, 0, len - 1));
 }
