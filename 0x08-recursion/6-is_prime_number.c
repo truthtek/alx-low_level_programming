30d835062f50413e9ad58e69c1a554122f14e9e9
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-30 14:18:20 -0700

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,13 +3,12 @@
 /**
  * is_prime_number - Checks if a number is a prime number
  * @n: The number to be checked
- *
- 
+ * 
  * Return: 1 if n is a prime number, 0 otherwise
  */
 int is_prime_helper(int n, int i)
 {
-/* function body */
+/* function helper */
 if (n <= 1)
 {
 return (0);
