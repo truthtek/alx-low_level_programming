0cbe65080d41fb616e65a18e27cd0f87aba39010
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-30 14:15:08 -0700

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,15 +15,15 @@ return (1);
 }
 if (*s1 == *s2)
 {
-return wildcmp(s1 + 1, s2 + 1);
+return (wildcmp(s1 + 1, s2 + 1));
 }
 if (*s2 == '*')
 {
 if (*(s2 + 1) == '*')
 {
-return wildcmp(s1, s2 + 1);
+return (wildcmp(s1, s2 + 1));
 }
-return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
 return (0);
 }
