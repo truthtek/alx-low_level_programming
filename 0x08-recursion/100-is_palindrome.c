c09ce398938426d4b817f9d0c10696e4bb2f7585
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-30 13:49:07 -0700

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: The string to be checked
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
+ int is_palindrome_helper(char *s, int start, int end)
+{
+if (start >= end)
+{
+return 1;
+}
+if (s[start] != s[end])
+{
+return 0;
+}
+return is_palindrome_helper(s, start + 1, end - 1);
+}
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return 0;
+}
+return 1 + _strlen_recursion(s + 1);
+}
+
+int is_palindrome(char *s)
+{
+int len = _strlen_recursion(s);
+return is_palindrome_helper(s, 0, len - 1);
+}
