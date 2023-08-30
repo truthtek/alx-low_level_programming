2e0dc8b88e25b2ac3ce5fba4b97c6ba7f4c6314d
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-30 13:55:29 -0700

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Calculates the natural square root of a number using recursion
+ * _sqrt_recursion - Calculates the natural square root of a number using
  * @n: The number for which square root is to be calculated
  *
- * Return: The natural square root of n, or -1 if n does not have a natural square root
+ * Return: The natural square root of n, or -1 if n does not have a natural
  */
 int _sqrt_helper(int n, int i)
 {
@@ -16,7 +16,7 @@ if (i * i > n)
 {
 return (-1);
 }
-return _sqrt_helper(n, i + 1);
+return (_sqrt_helper(n, i + 1));
 }
 int _sqrt_recursion(int n)
 {
@@ -24,5 +24,5 @@ if (n < 0)
 {
 return (-1);
 }
-return _sqrt_helper(n, 0);
+return (_sqrt_helper(n, 0));
 }
