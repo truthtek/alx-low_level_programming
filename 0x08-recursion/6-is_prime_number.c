f436a4397ea0bfceaca850e053ddf4317dff1ad7
truthtek <126268337+truthtek@users.noreply.github.com>
2023-08-31 15:12:29 -0700

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * is_prime_number - Checks if a number is a prime number
- * @n: The number to be checked
+ * set_string - Sets the value of a pointer to a given string.
+ * @s: Pointer to a pointer to a char (the target pointer).
+ * @to: Pointer to a char (the string to set the target pointer to).
  *
- * Return: 1 if n is a prime number, 0 otherwise
+ * This function dynamically allocates memory for the target string and updates
+ * the target pointer to point to the new string. It also frees the previous
+ * memory to prevent memory leaks.
  */
 int is_prime_helper(int n, int i)
 {
