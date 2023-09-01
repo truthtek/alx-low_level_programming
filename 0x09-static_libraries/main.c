c145d1f76de2b3570453984f7bfa2103d6eff745
truthtek <126268337+truthtek@users.noreply.github.com>
2023-09-01 11:51:24 -0700

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,1 +1,13 @@
-gcc -std=gnu89 main.c -L. -lmy -o quote
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
