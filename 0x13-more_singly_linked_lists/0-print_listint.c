acce4b9daf2191a278c9279172d86a318ae8140a
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-09-27 16:55:17 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,22 @@
-#include "my_linked_list.h" // Assuming "my_linked_list.h" is your own header file.
+#include "lists.h"
 
 /**
- * display_linked_list - displays all the elements of a custom linked list
- * @head: pointer to the head of the list
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
  *
- * Returns: the count of nodes in the list
+ * Return: the number of nodes
  */
-size_t display_linked_list(const my_node_t *head)
+size_t print_listint(const listint_t *h)
 {
-    const my_node_t *current_node = head;
-    size_t node_count = 0;
+    const listint_t *current = h;
+    size_t count = 0;
 
-    while (current_node != NULL)
+    while (current != NULL)
     {
-        printf("%d\n", current_node->data);
-        node_count += 1;
-        current_node = current_node->next;
+        printf("%d\n", current ->n);
+        count += 1;
+        current = current ->next;
     }
 
-    return (node_count);
+    return count;
 }
