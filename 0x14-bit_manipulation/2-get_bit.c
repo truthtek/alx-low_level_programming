fb1060306a470f29f06122ba822f327dedad584b
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-09-28 22:04:49 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,28 +11,15 @@
  *
  * Return: return 1 or 0
  */
-int get_bit(unsigned long int n, unsigned int index) {
-    if (index >= (sizeof(unsigned long int) * 8)) {
-        return -1;  // Error: index out of range
-    }
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (n == 0 && index == 0)
+		return (0);
+	if (index > (8 * 8))
+		return (-1);
 
-    unsigned long int mask = 1UL << index;
-    int bit_value = (n & mask) ? 1 : 0;
-
-    return bit_value;
-}
-
-int main() {
-    unsigned long int num = 10;  // Binary: 1010
-    int index = 2;  // Bit index to retrieve
-    
-    int bit_value = get_bit(num, index);
-
-    if (bit_value != -1) {
-        printf("The bit value at index %d is: %d\n", index, bit_value);
-    } else {
-        printf("Error: Index out of range.\n");
-    }
-
-    return 0;
+	if (n & (1 << index))
+		return (1);
+	else
+		return (0);
 }
