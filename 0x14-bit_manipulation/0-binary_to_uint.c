95121720e7e317df9200e634e6c856b266d99386
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-10-26 21:17:52 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,19 +7,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int num = 0;
-
-  if (b == NULL)
-    return 0;
-
-  while (*b) {
-    if (*b != '0' && *b != '1')
-      return 0;
-    
-    num <<= 1;
-    num += (*b - '0');
-    b++;
-  }
-
-  return num;
+unsigned int num = 0;
+if (b == NULL)
+return (0);
+while (*b) {
+if (*b != '0' && *b != '1')
+return (0);    
+num <<= 1;
+num += (*b - '0');
+b++;
+}
+return (num);
 }
