c7406465da1947a8ea2c72349479ba8bf15db3dd
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-11-03 10:01:06 +0000

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,48 +2,48 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - Reads a text file and prints it to stdout
+ * @filename: A pointer to the filename to read
+ * @letters: The number of letters to read and print
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
- */
+ * Return: Actual number of letters read and printed.
+ * 0 if filename is NULL or fail to read/write
+ */ 
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+        int fd;
+        ssize_t num_read, num_written;
+        char *buf;
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    ssize_t o, r, w;
-    char *buffer;
+        if (filename == NULL)
+                return (0);
 
-    if (filename == NULL)
-        return (0);
+        fd = open(filename, O_RDONLY);
+        if (fd == -1)
+                return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+        buf = malloc(sizeof(char) * letters);
+        if (buf == NULL)
+                return (0);
 
-    o = open(filename, O_RDONLY);
-    if (o == -1) {
-        free(buffer);
-        return (0);
-    }
+        num_read = read(fd, buf, letters);
+        if (num_read == -1)
+        {
+                free(buf);
+                close(fd);
+                return (0);
+        }
 
-    r = read(o, buffer, letters);
-    if (r == -1) {
-        free(buffer);
-        close(o);
-        return (0);
-    }
+        num_written = write(STDOUT_FILENO, buf, num_read);
+        if (num_written == -1 || num_written != num_read)
+        {
+                free(buf);
+                close(fd);
+                return (0);
+        }
 
-    w = write(STDOUT_FILENO, buffer, r);
-    if (w == -1 || w != r) {
-        free(buffer);
-        close(o);
-        return (0);
-    }
+        free(buf);
+        close(fd);
 
-    free(buffer);
-    close(o);
-
-    return (w);
+        return (num_written);
 }
