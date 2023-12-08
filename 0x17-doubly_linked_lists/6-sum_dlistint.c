943f4ed4d1e8b9b92e5d8265e2bcf35059b7b2bf
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-08 15:57:15 +0100

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -8,13 +8,11 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
-
-    while (head != NULL)
-    {
-        sum += head->n;
-        head = head->next;
-    }
-
-    return sum;
+int sum = 0;
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
