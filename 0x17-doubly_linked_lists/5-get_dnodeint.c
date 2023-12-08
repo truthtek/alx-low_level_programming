4c03c28cecef020f4917ef72cfb2a91b55a1a029
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-08 15:55:10 +0100

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,16 +10,13 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int count = 0;
-
-    while (head != NULL)
-    {
-        if (count == index)
-            return head;
-
-        head = head->next;
-        count++;
-    }
-
-    return NULL;
+unsigned int count = 0;
+while (head != NULL)
+{
+if (count == index)
+return (head);
+head = head->next;
+count++;
+}
+return (NULL);
 }
