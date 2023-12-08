9eb6e93ccf33d014038d225ddd62fbd143c7a9c4
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-08 16:09:48 +0100

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,40 +10,31 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp, *to_delete;
-    unsigned int count = 0;
-
-    if (head == NULL || *head == NULL)
-        return (-1);
-
-    if (index == 0)
-    {
-        to_delete = *head;
-        *head = (*head)->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(to_delete);
-        return (1);
-    }
-
-    temp = *head;
-
-    while (temp != NULL && count < index - 1)
-    {
-        temp = temp->next;
-        count++;
-    }
-
-    if (temp == NULL || temp->next == NULL)
-        return (-1);
-
-    to_delete = temp->next;
-    temp->next = to_delete->next;
-
-    if (to_delete->next != NULL)
-        to_delete->next->prev = temp;
-
-    free(to_delete);
-
-    return (1);
+dlistint_t *temp, *to_delete;
+unsigned int count = 0;
+if (head == NULL || *head == NULL)
+return (-1);
+if (index == 0)
+{
+to_delete = *head;
+*head = (*head)->next;
+if (*head != NULL)
+(*head)->prev = NULL;
+free(to_delete);
+return (1);
+}
+temp = *head;
+while (temp != NULL && count < index - 1)
+{
+temp = temp->next;
+count++;
+}
+if (temp == NULL || temp->next == NULL)
+return (-1);
+to_delete = temp->next;
+temp->next = to_delete->next;
+if (to_delete->next != NULL)
+to_delete->next->prev = temp;
+free(to_delete);
+return (1);
 }
