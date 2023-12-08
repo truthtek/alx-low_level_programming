417886a12d9b6bdbbb5fddf764ccc5f1ebf35b5f
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2023-12-08 15:52:19 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,12 +7,11 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
-
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+dlistint_t *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
 }
