68e7338f8636aedcd26e9aa8cb231bb311c84891
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-09 03:17:00 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,21 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-void print_number(int n) {
-    if (n == 0) {
-        _putchar('0');
-        return;
-    }
-
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    int digits[10];
-    int i = 0;
-
-    while (n > 0) {
-        digits[i++] = n % 10;
-        n /= 10;
-    }
-
-    while (i > 0) {
-        _putchar('0' + digits[--i]);
-    }
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+ 	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
 }
