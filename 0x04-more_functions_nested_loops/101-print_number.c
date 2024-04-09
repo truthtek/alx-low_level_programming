cf188ec1707b7321165cc57328f7d2870cf55e83
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-09 03:23:43 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,21 +1,48 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * print_number - print an integer input into the function
+ * @n: the user input of the  number to be printed
+ * Return: no value
  */
-int main(void)
+void print_number(int n)
 {
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
- 	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
-	return (0);
+	int i = 1, first;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (-(n + 1));
+	}
+	else
+		n = (n - 1);
+	first = (n % 10);
+	while ((n / i) > 9)
+		i *= 10;
+	while (i != 1 || n == 9)
+	{
+		if ((n + 1) == (i * 10))
+		{
+			n += 1, i *= 10, _putchar((n / i) + '0');
+			while (i != 1)
+				n /= 10, i /= 10, _putchar((i % n) + '0');
+			return;
+		}
+		else
+		{
+			if (i > n)
+				_putchar(((n / n) - 1) + '0');
+			else
+			{
+				_putchar((n / i) + '0');
+				n = (n % i);
+			}
+			i /= 10;
+		}
+	}
+	if (first < 9)
+		first += 1;
+	else
+		first = 0;
+	_putchar((first) + '0');
 }
