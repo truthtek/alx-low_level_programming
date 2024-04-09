68557dba655bee2cc48e9fc44279b616bda74e7b
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-09 03:52:31 +0100

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,16 +8,13 @@
  */
 int pop_listint(listint_t **head)
 {
-    listint_t *temp;
-    int data;
-
-    if (*head == NULL)
-        return 0;
-
-    temp = *head;
-    data = temp->n;
-    *head = (*head)->next;
-    free(temp);
-
-    return data;
+listint_t *temp;
+int data;
+if (*head == NULL)
+return (0);
+temp = *head;
+data = temp->n;
+*head = (*head)->next;
+free(temp);
+return (data);
 }
