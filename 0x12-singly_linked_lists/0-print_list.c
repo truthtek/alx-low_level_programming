1de57a4713e0d9a72bfec1dd6e1da18e4ecb92fd
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-09 04:13:32 +0100

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+// print_list.c
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+    size_t nodes = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+
+        h = h->next;
+        nodes++;
+    }
+
+    return nodes;
+}
