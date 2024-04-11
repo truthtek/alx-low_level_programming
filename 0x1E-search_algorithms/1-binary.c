bdc0393a5017923a893f1b34c4fe781acaa3205c
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-11 21:15:02 +0100

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,57 +1,37 @@
-#include <stdio.h>
+
 #include "search_algos.h"
 
 /**
- * print_array - Prints an array of integers
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(int *array, size_t size)
-{
-    size_t i;
-
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-        else
-            printf("\n");
-    }
-}
-
-/**
- * binary_search - Searches for a value in a sorted array of integers
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in array
- * @value: Value to search for
+ * binary_search - performs binary search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
  *
- * Return: The index where value is located, or -1 on failure
+ * Return: the index found or -1
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t mid;
-    int res;
+	size_t i = 0;
+	int *a = array;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    print_array(array, size);
-    if (size == 1 && array[0] != value)
-        return (-1);
+	while (size)
+	{
+		for (i = 0, printf("Searching in array: "); i < size; i++)
+			printf("%d%s", a[i], i + 1 == size ? "\n" : ", ");
 
-    mid = (size - 1) / 2;
-    if (array[mid] == value)
-        return (mid);
-    else if (array[mid] > value)
-        return (binary_search(array, mid + 1, value));
-    else
-    {
-        res = binary_search(array + mid + 1, size - mid - 1, value);
-        if (res == -1)
-            return (-1);
-        else
-            return (res + mid + 1);
-    }
+		i = (size - 1) / 2;
+		if (a[i] == value)
+			return ((a - array) + i);
+		else if (a[i] > value)
+			size = i;
+		else
+		{
+			a += (i + 1);
+			size -= (i + 1);
+		}
+	}
+	return (-1);
 }
