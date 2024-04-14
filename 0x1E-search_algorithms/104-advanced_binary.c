784c3078fd826593404ac6065f4d44cfee45e58f
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-04-14 21:07:53 +0100

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,75 +1,50 @@
+
 #include "search_algos.h"
 
 /**
- * advanced_binary - searches for a value in a sorted array of integers
- *  using the Binary search algorithm
- *
- * @array: a pointer to the first element of the array to search in
- * @size: number of elements in array
+ * _advanced_binary - performs advanced binary search
+ * @array: the integer array
+ * @size: its size
  * @value: value to search for
- * Return:  first index where value is located or -1 if value is not
- * present in array or if array is NULL
+ *
+ * Return: the index found or -1
+ */
+int *_advanced_binary(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!size || !array)
+		return (NULL);
+	for (printf("Searching in array: "); i < size; i++)
+		printf("%d%s", array[i], i + 1 == size ? "\n" : ", ");
+
+	i = (size - 1) / 2;
+	if (array[i] == value)
+	{
+		if (i)
+			return (_advanced_binary(array, i + 1, value));
+		return (array + i);
+	}
+	else if (array[i] > value)
+		return (_advanced_binary(array, i + 1, value));
+	else
+		return (_advanced_binary(array + i + 1, size - i - 1, value));
+}
+
+/**
+ * advanced_binary - performs advanced binary search
+ * @array: the integer array
+ * @size: its size
+ * @value: value to search for
+ *
+ * Return: the index found or -1
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t start, end;
+	int *a = _advanced_binary(array, size, value);
 
-	if (array == NULL)
+	if (!a)
 		return (-1);
-
-	start = 0;
-	end = size - 1;
-	return (adv_bin_helper(array, start, end, value));
-}
-
-/**
- * adv_bin_helper - searches for a value in a sorted array of integers using
- * recursive Binary Search
- *
- * @array: a pointer to the first element of the array to search in
- * @start: index of starting element
- * @end: index of ending element
- * @value: value to search for
- * Return:  first index where value is located or -1 if value is not
- * present in array or if array is NULL
- */
-int adv_bin_helper(int *array, size_t start, size_t end, int value)
-{
-	size_t mid;
-
-	if (start > end)
-		return (-1);
-
-	printf("Searching in array: ");
-	print_array(array, start, end);
-
-	mid = start + (end - start) / 2;
-	if (value == array[mid] && value != array[mid - 1])
-		return (mid);
-	else if (value <= array[mid])
-		return (adv_bin_helper(array, start, mid, value));
 	else
-		return (adv_bin_helper(array, mid + 1, end, value));
-}
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @start: First element in array
- * @end: Last element in array
- */
-void print_array(const int *array, size_t start, size_t end)
-{
-	size_t i;
-
-	i = start;
-	while (array && i <= end)
-	{
-		if (i > start)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
+		return (a - array);
 }
